4602efe89f905601ffe54954aa317463868517ab
husarz <husarzdev@gmail.com>
2017-12-10 01:00:31 +0100

    zmiana publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -87,8 +87,8 @@ module.exports = {
     ]
   },
   "output": {
-    "path": path.join(process.cwd(), "wwwroot"),
-    "publicPath": "wwwroot/", // Webpack dev middleware, if enabled, handles requests for this URL prefix
+    "path": path.join(process.cwd(), "wwwroot"), //path: The output directory as an absolute path (required); process.cwd: method returns the current working directory of the Node.js process.
+    "publicPath": "/wwwroot/", //było wwwroot/ // Webpack dev middleware, if enabled, handles requests for this URL prefix
     "filename": "[name].bundle.js",
     "chunkFilename": "[id].chunk.js"
   },
